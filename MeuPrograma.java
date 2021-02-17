d8fbe25a33c06202e7c8c3096f3fa0b621809e0b
fabiogaviao <gaviao_tsi@uol.com.br>
2021-02-16 21:29:53 -0300

    melhoria dos comentarios

--- MeuPrograma.java
+++ MeuPrograma.java
@@ -1,8 +1,14 @@
-
-class Saudacao{
+// class indica para o compilador que o próximo item será uma classe.
+class Saudacao{  // o corpo da classe começa com o { e termina com o }
 	// aqui você começa a escrever o seu programa
-	public static void main(String[] args){
+	public static void main(String[] args){ // método inicial que todo programa Java tem que ter.
+		// void quer dizer que o main		// Seu corpo começa com { e encerra no }
+		// não retorna nada.				// Dentro do { e } a gente escreve as instruções propriamente
+		// String é uma classe.				// ditas do nosso programa.
+		// args é o argumento(nome do argumento) [] indica que args é um vetor de objetos do tipo String.
+		// Chamar o método println para imprimir a frase que está dentro do parênteses na tela
 		System.out.println("Meu primeiro programa Java.\n");
-	}
+		// System é uma classe. out é uma subclasse. println é um método da classe out.
+	} // Fim do método main
 	// aqui termina o seu programa
-}
+} // fim da classe Saudacao.
