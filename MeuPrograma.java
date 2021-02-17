901b7c24820e73163b70608da5f474067646e05f
fabiogaviao <gaviao_tsi@uol.com.br>
2021-02-16 21:02:54 -0300

    importação inicial

---
+++ MeuPrograma.java
@@ -0,0 +1,8 @@
+
+class Saudacao{
+	// aqui você começa a escrever o seu programa
+	public static void main(String[] args){
+		System.out.println("Meu primeiro programa Java.\n");
+	}
+	// aqui termina o seu programa
+}
